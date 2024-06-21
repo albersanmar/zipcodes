3aaff06d792f967dec84c7e92f724661831a3f5b
albersanmar <albersanmar4@gmail.com>
2024-06-21 14:25:10 -0600

    Order by

--- src/app/services/states_service.js
+++ src/app/services/states_service.js
@@ -2,7 +2,11 @@ const State = require("../../app/models/State");
 
 class StatesService {
   static async list() {
-    const states = await State.findAll();
+    const states = await State.findAll({
+      order: [
+        ['id', 'ASC']
+      ]
+    });
     return states;
   }
   static async find(id) {
